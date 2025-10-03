6dec5d57d0bff6156c9afe50c3ed55f16f624540
H4ROOON <haroonadeel69@gmail.com>
2025-10-03 21:01:09 +0500

    add basic skeleton

---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,8 @@
+export default function Scoreboard({ score, bestScore }) {
+  return (
+    <div className="scoreboard">
+      <p>Score: {score}</p>
+      <p>Best Score: {bestScore}</p>
+    </div>
+  );
+}
